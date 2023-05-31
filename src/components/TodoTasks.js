41f6fe389e21cc00a94ae383b65606132e05acf7
Oliver <64933659+OliverSla@users.noreply.github.com>
2023-05-31 02:41:42 +0200

    Update TodoTasks.js
    React Todo App - Skúška commitu na mobile

--- src/components/TodoTasks.js
+++ src/components/TodoTasks.js
@@ -6,11 +6,12 @@ const TodoTasks = () => {
         <div className='todoTasksLine'></div>
         <div className='tasks'>
             <div className="oneTask">
+                <div className="taskCheck"></div>
                 <h3>Vyniesť smetie</h3>
+                <div className="options"><>
             </div>
 
             <div className="oneTask">
-                
                 <h3>Upratať izbu</h3>
             </div>
         </div>
